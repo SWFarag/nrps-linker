8ea2d97bf1c89a14d1ba8acc7c5e50f175290202
bifa2005 <bifa2005@gmail.com>
2017-06-30 21:24:10 -0400

    Bacitracin A1 capital letter

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '5'))
+print "WORKER", workers
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '1000'))
 
